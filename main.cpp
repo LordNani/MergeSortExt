c81631354c50c5bb2b9099a0fbcdfc711f9a85cf
LordNani <rigansu@gmail.com>
2019-10-20 21:42:16 +0300

    Some bug

--- main.cpp
+++ main.cpp
@@ -2,40 +2,46 @@
 #include <time.h>
 using namespace std;
 void fillArray(int *pArr, size_t s) {
+  cout << "Unsorted array: ";
   for (int i = 0; i < s; i++) {
-	  pArr[i] = rand() % (2 * s);
-	  cout << pArr[i] << endl;
+    pArr[i] = rand() % (2 * s);
+    cout << pArr[i] << ", ";
   }
 }
 // TODO: clear memory
 
-int* mergeSort(int* pMain, size_t s) {
-	int middle = s / 2;
-	int* pLeft = pMain;
-	int* pRight = pMain + middle; // may cause some troubles
-	return merge(pLeft, middle, pRight, s - middle + 1); //aybe here + 0
+int* mergeSort(int *pMain, size_t s) {
+  if (s < 2) {
+    return pMain;
+  }
+  size_t middle = s / 2;
+  int *pLeft = pMain;
+  int *pRight = pMain + middle; // may cause some troubles
+  return merge(mergeSort(pLeft, middle), middle,
+               mergeSort(pRight, s - middle + 1),
+               s - middle + 1); // aybe here + 0
 }
 
-int* merge(int* left, size_t l_length, int* right, size_t r_length) {
-	int i, j, m;
-	int* result = new int[l_length+r_length];
-	while (i < l_length && j < r_length) {
-		if (left[i] < right[j]) {
-			result[m] = left[i];
-			i++;
-		}
-		else {
-			result[m] = right[j];
-			j++;
-		}
-		m++;
-	}
-	for (m;m < l_length + r_length;m++) {
-		while (i < l_length)
-			result[m] = left[i];
-		while (j < r_length)
-			result[m] = right[j];
-	}
+int* merge(int *left, size_t l_length, int *right, size_t r_length) {
+  int i, j, m;
+  int *result = new int[l_length + r_length];
+  while (i < l_length && j < r_length) {
+    if (left[i] < right[j]) {
+      result[m] = left[i];
+      i++;
+    } else {
+      result[m] = right[j];
+      j++;
+    }
+    m++;
+  }
+  for (m; m < l_length + r_length; m++) {
+    while (i < l_length)
+      result[m] = left[i];
+    while (j < r_length)
+      result[m] = right[j];
+  }
+  return result;
 }
 
 int main() {
@@ -48,5 +54,9 @@ int main() {
 
   fillArray(mainArr, static_cast<size_t>(n));
   resultArr = mergeSort(mainArr, static_cast<size_t>(n));
+  cout << "RESULT ARR: ";
+  for (int i = 0; i < n; i++) {
+    cout << resultArr[i] << ", ";
+  }
   return 0;
 }
